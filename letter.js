19095bc34dcf6ca46a35b1030a7a1da5cc975584
aOpenbrier <aopenbrier@gmail.com>
2018-10-13 00:34:00 -0700

    initial files

---
+++ letter.js
@@ -0,0 +1,5 @@
+// constructor for letter objects containing:
+//  - a string of the letter
+//  - a boolean if letter is guessed yet
+//  - a function which return stored string if guessed, or placeholder symbol
+//  - a function which takes guess as an argument and checks if it matches letter string
---
+++ word.js
@@ -0,0 +1,4 @@
+//contructor for word objects containing:
+//  -An array of new Letter objects representing word
+//  -A function that returns a string representing the word, using Letter prototype function
+//  -A function that takes a character as an argument and calls the guess function on each letter object
