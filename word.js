47a29303475995479229f9c391c4144a25d44f3a
aOpenbrier <aopenbrier@gmail.com>
2018-10-15 01:11:42 -0700

    word constructor

--- word.js
+++ word.js
@@ -1,4 +1,31 @@
-//contructor for word objects containing:
-//  -An array of new Letter objects representing word
-//  -A function that returns a string representing the word, using Letter prototype function
-//  -A function that takes a character as an argument and calls the guess function on each letter object
+const Letter = require('./letter.js')
+
+// Contructor for word object:
+const Word = function (fullword) {
+    this.addLetters = function () {
+        let letterArr = []
+        fullword.split('').forEach(function (item) {
+            letterArr.push(new Letter(item))
+        })
+        return letterArr
+    }
+    //  An array of new Letter objects representing the word
+    this.arr = this.addLetters()
+    console.log(this.arr.join(''))
+}
+
+//  function that returns a string representation the word
+Word.prototype.toString = function () {
+    return this.arr.join('')
+}
+
+//  function that checks the guess input on each letter object
+Word.prototype.checkGuess = function (guess) {
+    this.arr.forEach(item => item.checkGuess(guess))
+}
+
+let newword = new Word('pizza')
+newword.checkGuess('z')
+console.log(newword)
+
+module.exports = Word
